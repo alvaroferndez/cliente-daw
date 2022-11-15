98de3fa752d59e09caff4d18ed83ab8629023997
alvaro fernandez domingo <alvarofdayala@gmail.com>
2022-11-15 21:17:26 +0100

    pong solo falta añadir tope para las barras

--- tema 5/svg/pong/index.js
+++ tema 5/svg/pong/index.js
@@ -1,120 +1,41 @@
-function iniciar(){
-      //contador 
-      jugador1 = document.getElementsByTagName("span")[0];
-      jugador2 = document.getElementsByTagName("span")[2];
-      puntos_jugador1 = 0;
-      puntos_jugador2 = 0;
+import {Bola} from "./bola.js";
+import {Jugador} from "./jugador.js";
 
+function iniciar(){
       //bola
-      bola = document.getElementById("bola");
-      cx = 50;
-      cy = 50;
-      radio = 10;
-      direccionx=1;
-      direcciony=1;
-      speedx = 3;
-      speedy = 3;
+      let bola = new Bola();
+      bola.crearElemento();
 
       //barras
-      barra1 = document.getElementById("barra1");
-      barra2 = document.getElementById("barra2");
-      x1 = parseInt(barra1.getAttribute("x"));
-      y1 = 125;
-      x2 = parseInt(barra2.getAttribute("x"));
-      y2 = 125;
-      direcciony1=1;
-      direcciony2=1;
+      let jugador1 = new Jugador(10,125,1);
+      let jugador2 = new Jugador(980,125,2);
+      jugador1.crearElemento();
+      jugador2.crearElemento();
 
-      function cargarSesiones(){
-            if(sessionStorage.jugador1 && sessionStorage.jugador2){
-                  jugador1.innerHTML = JSON.parse(sessionStorage.jugador1);
-                  jugador2.innerHTML = JSON.parse(sessionStorage.jugador2);
-                  puntos_jugador1 = JSON.parse(sessionStorage.jugador1);
-                  puntos_jugador2 = JSON.parse(sessionStorage.jugador2);
-            }else{
-                  sessionStorage.jugador1 = JSON.stringify(0);
-                  sessionStorage.jugador2 = JSON.stringify(0);
-            }     
+      //cargamos la puntuacion de cada jugador
+      jugador1.cargarPuntuacion();
+      jugador2.cargarPuntuacion();
+      
+      //añadimos el movimiento a las barras
+      jugador1.movimientoBarra();
+      jugador2.movimientoBarra();
+
+      //añadimos el movimiento a la bola
+      function callbackDibujarBola(){
+            bola.dibujarBola(jugador1,jugador2)
       }
-
-      cargarSesiones();
-
-      function movimientoBola(){
-
-            function sumarJugador1(){
-                  puntos_jugador2++;
-                  jugador2.innerHTML = puntos_jugador2;
-                  sessionStorage.jugador2 = JSON.stringify(puntos_jugador2);
-                  cx=500;
-            }
-
-            function sumarJugador2(){
-                  puntos_jugador1++;
-                  jugador1.innerHTML = puntos_jugador1;
-                  sessionStorage.jugador1 = JSON.stringify(puntos_jugador1);
-                  cx=500;
-            }
-
-
-            function dibujarBola(){
-                  if(cx>(1000-radio) || cx<(0+radio)){
-                        if(cx>(1000-radio)){
-                              sumarJugador2();
-                        }else if(cx<(0+radio)){
-                              sumarJugador1();
-                        }
-                  }
-
-                  if(cy>(300-radio) || cy<(10+radio)){
-                        direcciony*=-1;
-                  }
-
-                  if((cx-(radio*2)) < x1 && (cy<=(y1 +50) && cy>= y1)){
-                        direccionx*=-1;
-                  }
-
-
-                  if((cx+radio) > x2 && (cy<=(y2 +50) && cy>= y2)){
-                        direccionx*=-1;
-                  }
-
-                  cx += direccionx*speedx;
-                  bola.setAttribute("cx",cx);
-
-                  cy += direcciony*speedy;
-                  bola.setAttribute("cy",cy);
-            }
-            
-
-
-            intervalo = setInterval(dibujarBola, 10);
-      }
-
-      function movimientoBarra(){
-            document.addEventListener("keydown", (e) => {
-                  if(e.key == "w"){
-                        y1 += -10;
-                        barra1.setAttribute("y",y1);
-                  }else if(e.key == "s"){
-                        y1 += 10;
-                        barra1.setAttribute("y",y1);
-                  }else if(e.key == "ArrowUp"){
-                        y2 += -10;
-                        barra2.setAttribute("y",y2);
-                  }else if(e.key == "ArrowDown"){
-                        y2 += 10;
-                        barra2.setAttribute("y",y2);
-                  }
-            })
-      }
-
-      movimientoBola();
-      movimientoBarra();
+      setInterval(callbackDibujarBola, 10);
 }
 
 
 
 
+
 window.addEventListener("load", () => {
-      iniciar()
+      var botones = document.getElementsByTagName("button");
+      botones[0].addEventListener("click", iniciar);
+      botones[1].addEventListener("click", () => {
+            sessionStorage.clear();
+            location.reload();
+      })
 })
